5d666fe8e45ab4f60ce41e9fe3c9e72a95d65606
Manolo Martinez <rosso.neeri@gmail.com>
2020-04-27 13:02:53 -0500

    #fix [@itia-digital/connect/lib/@connect] => [@itia-digital/connect]

--- projects/idg-connect/src/public-api.ts
+++ projects/idg-connect/src/public-api.ts
@@ -2,9 +2,9 @@
  * Public API Surface of idg-connect
  */
 
-export * as notifications from './lib/@notifications';
-export * as connect from './lib/@connect';
-export * as rxjs from './lib/@rxjs';
-export * as app from './lib/@app';
+export * from './lib/@notifications';
+export * from './lib/@connect';
+export * from './lib/@rxjs';
+export * from './lib/@app';
 
 export * from './lib/idg-connect.module';
